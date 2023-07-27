1a1a4e536c5d42e07a08802461076990f0af97ba
mphoChuene <mphochuene42@gmail.com>
2023-07-27 11:49:09 +0200

    functionality completed

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-document.getElementById('counter-display').innerText=12
+let count = 0;
+
+document.getElementById("increaseBtn").onclick = function increase() {
+  count = count + 1;
+  document.getElementById("counterDisplay").innerHTML = count;
+};
+
+document.getElementById("resetBtn").onclick = function reset() {
+  count = 0;
+  document.getElementById("counterDisplay").innerHTML = count;
+};
+document.getElementById("decreaseBtn").onclick = function decrease() {
+  count = count - 1;
+  document.getElementById("counterDisplay").innerHTML = count;
+};
